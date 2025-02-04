e14bcf7b1fb13baa8e96dec4aed986c420b83191
Nico Hede <nico.hede@esedulainen.fi>
2025-02-04 11:58:21 +0200

    Css kuntoon ja nav bar suuri muokkaus uutiset kohtaan kehitteillä uusi nav bar sivu toistaiseksi kesken, responsiivisuus laitettu koko sivulle!

---
+++ script/script.js
@@ -0,0 +1,25 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menuToggle");
+    const sidebarMenu = document.getElementById("sidebarMenu");
+    const overlay = document.getElementById("overlay");
+
+    menuToggle.addEventListener("click", function () {
+        // Tarkistetaan onko menu jo auki
+        if (sidebarMenu.classList.contains("open")) {
+            sidebarMenu.classList.remove("open");
+            overlay.classList.remove("active");
+        } else {
+            sidebarMenu.classList.add("open");
+            overlay.classList.add("active");
+        }
+    });
+
+    overlay.addEventListener("click", function () {
+        sidebarMenu.classList.remove("open");
+        overlay.classList.remove("active");
+    });
+});
+
